fb410473fe67513817b21b56987e1c6592942ea3
Luca Giovannini <75088659+lucagiovannini7@users.noreply.github.com>
2023-01-22 13:28:19 +0100

    Update script.js

--- map/script.js
+++ map/script.js
@@ -5,7 +5,7 @@ let mapOptions = {
 
 let map = new L.map('map' , mapOptions);
 
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+let layer = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 map.addLayer(layer);
 
 let locations = [
@@ -15,28 +15,6 @@ let locations = [
 "long": -1.264856,
 "title": "Erasmus+ stay, 2014-15",
 "url":"https://www.warwick.ac.uk/"
-},
-{
-"id": 2,
-"lat": 45.070312,
-"long": 7.6868565,
-"title1": "Bachelors' degree, 2014-17",
-"title2": "Masters' degree, 2017-20",
-"url":"https://www.unito.it/"
-},
-{
-"id": 3,
-"lat": 50.938361,
-"long": 6.959974,
-"title": "Erasmus+ stay, 2017-18",
-"url":"https://www.uni-koeln.de/"
-},
-{
-"id": 4,
-"lat": 52.391703,
-"long": 13.064349,
-"title": "Doctoral degree, 2021-",
-"url":"https://www.uni-potsdam.de/"
 }
 ]
 
@@ -47,12 +25,12 @@ for ( let i = 0 ; i< locations.length ; i++){
 
 markers.forEach(marker => {
     marker.on("mouseover",event =>{
-        event.target.bindPopup('<div class="card"> <h3>'+element.title+'</h3></div>').openPopup();
+        event.target.bindPopup('<div class="card"> <h3>'+marker.options.title+'</h3></div>').openPopup();
     })
     .on("mouseout", event => {
         event.target.closePopup();
     })
     .on("click" , () => {
-        window.open(element.url);
+        window.open(marker.options.url);
     })
 });
