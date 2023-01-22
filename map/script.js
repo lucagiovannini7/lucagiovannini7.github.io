bdcb0b69d294ec68af7cccd928e886d79483b2e8
Luca Giovannini <75088659+lucagiovannini7@users.noreply.github.com>
2023-01-22 12:42:33 +0100

    Update script.js

--- map/script.js
+++ map/script.js
@@ -9,5 +9,26 @@ let map = new L.map('map' , mapOptions);
 let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 map.addLayer(layer);
 
-let marker = new L.Marker([51.958, 9.141]);
-marker.addTo(map);
+let locations = [
+    {
+        "id": 1,
+        "lat": 45.070312,
+        "long": 7.6868565,
+        "title": "Bacherlor's degree, 2014-2017"
+    }
+	]
+	
+for ( let i = 0 ; i< locations.length ; i++){
+ new L.Marker([element.lat,element.long]).addTo(map)
+}
+
+
+locations.forEach(element => {
+    new L.Marker([element.lat,element.long]).addTo(map)
+    .on("mouseover",event =>{
+        event.target.bindPopup('content').openPopup();
+    })
+    .on("mouseout", event => {
+        event.target.closePopup();
+    })
+});
