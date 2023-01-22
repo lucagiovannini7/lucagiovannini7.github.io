e84848bf2a6b2f91e17bd1517c9a96638e3ea10a
Luca Giovannini <75088659+lucagiovannini7@users.noreply.github.com>
2023-01-22 14:31:05 +0100

    Update script.js

--- map/script.js
+++ map/script.js
@@ -1,6 +1,6 @@
 let mapOptions = {
-    center:[45.070312, 7.6868565],
-    zoom:10
+    center:[48.78232, 9.17702],
+    zoom:5
 }
 
 let map = new L.map('map' , mapOptions);
@@ -9,23 +9,45 @@ let layer = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 map.addLayer(layer);
 
 let locations = [
-{
-"id": 1,
-"lat": 52.378091,
-"long": -1.264856,
-"title": "Erasmus+ stay, 2014-15",
-"url":"https://www.warwick.ac.uk/"
-}
+    {
+        "id": 1,
+        "lat": 52.378091,
+        "long": -1.264856,
+        "title": "Erasmus+ stay, 2014-15",
+        "url":"https://www.warwick.ac.uk/"
+    },
+    {
+        "id": 2,
+        "lat": 45.070312,
+        "long": 7.6868565,
+        "title1": "Bachelors' degree, 2014-17",
+        "title2": "Masters' degree, 2017-20",
+        "url":"https://www.unito.it/"
+    },
+    {
+        "id": 3,
+        "lat": 50.938361,
+        "long": 6.959974,
+        "title": "Erasmus+ stay, 2017-18",
+        "url":"https://www.uni-koeln.de/"
+    },
+    {
+        "id": 4,
+        "lat": 52.391703,
+        "long": 13.064349,
+        "title": "Doctoral degree, 2021-",
+        "url":"https://www.uni-potsdam.de/"
+    }
 ]
 
 let markers = []
 for ( let i = 0 ; i< locations.length ; i++){
-    markers[i] = new L.Marker([locations[i].lat,locations[i].long], {title: locations[i].title, url: locations[i].url}).addTo(map);
+    markers[i] = new L.Marker([locations[i].lat,locations[i].long], {title1: locations[i].title1,title2: locations[i].title2, url: locations[i].url}).addTo(map);
 }
 
 markers.forEach(marker => {
     marker.on("mouseover",event =>{
-        event.target.bindPopup('<div class="card"> <h3>'+marker.options.title+'</h3></div>').openPopup();
+        event.target.bindPopup('<div class="card"> <h3>'+marker.options.title1+'</h3> <h3>'+ marker.options.title2+'</h3></div>').openPopup();
     })
     .on("mouseout", event => {
         event.target.closePopup();
