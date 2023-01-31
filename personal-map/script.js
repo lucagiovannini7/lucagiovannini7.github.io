826b14615d83a94729eae1984b871213f26a2550
Luca Giovannini <75088659+lucagiovannini7@users.noreply.github.com>
2023-01-31 17:23:25 +0100

    update

--- personal-map/script.js
+++ personal-map/script.js
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.script.ScriptEngine;
@@ -7,26 +7,25 @@ import javax.script.ScriptException;
 
 public class LeafletExample {
 
-    public static void main(String[] args) {
-        // Create a ScriptEngine object
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("javascript");
+    public static void main(String[] args) throws ScriptException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("javascript");
 
-        // Input the list of cities
-        List<String> cities = Arrays.asList("New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX", "Phoenix, AZ");
+        // Create a list of cities
+        List<String> cities = new ArrayList<>();
+        cities.add("New York, NY");
+        cities.add("Los Angeles, CA");
+        cities.add("Chicago, IL");
+        cities.add("Houston, TX");
+        cities.add("Phoenix, AZ");
 
-        try {
-            // Create a Leaflet map
-            engine.eval("var map = L.map('map').setView([37.8, -96], 4);");
-            engine.eval("L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'}).addTo(map);");
+        // Create a Leaflet map
+        engine.eval("var map = L.map('map').setView([37.8, -96], 4);");
+        engine.eval("L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'}).addTo(map);");
 
-            // Geocode the cities and add markers to the map
-            for (String city : cities) {
-                engine.eval("var geocoder = L.Control.Geocoder.nominatim();");
-                engine.eval("geocoder.geocode('" + city + "', function(results) {var r = results[0]; if (r) {L.marker([r.lat, r.lng]).addTo(map).bindPopup(r.name);}});");
-            }
-        } catch (ScriptException e) {
-            System.out.println("An error occurred: " + e.getMessage());
+        // Geocode the cities and add markers to the map
+        for (String city : cities) {
+            engine.eval("var geocoder = L.Control.Geocoder.nominatim();");
+            engine.eval("geocoder.geocode('" + city + "', function(results) {var r = results[0]; if (r) {L.marker([r.lat, r.lng]).addTo(map).bindPopup(r.name);}});");
         }
     }
 }
